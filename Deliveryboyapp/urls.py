9d3b0bc6cffb43e36110e32d73e240dcc8bb5585
Suhairmk <suhairsuha876@gmail.com>
2025-07-19 17:24:33 +0530

    chat

--- Deliveryboyapp/urls.py
+++ Deliveryboyapp/urls.py
@@ -16,12 +16,14 @@ urlpatterns = [
     path('deliveryBoyProfileupdateAPIView/',DeliveryBoyProfileAPIView.as_view(), name='deliveryBoyProfileupdateAPIView'),
     path('changePasswordAPIView/',ChangePasswordAPIView.as_view(), name='changePasswordAPIView'),
     path('postComplaintAPIView/',PostComplaintAPIView.as_view(), name='postComplaintAPIView'),
-    path('orderHistoryAPIView/',OrderHistoryAPIView.as_view(), name='orderHistoryAPIView'),  
+    path('orderHistoryAPIView/',OrderHistoryAPIView.as_view(), name='orderHistoryAPIView'),   
     path('feedbackAPIView/',FeedbackAPIView.as_view(), name='feedbackAPIView'),  
     path('forgotPasswordAPIView/',ForgotPasswordAPIView.as_view(), name='forgotPasswordAPIView'),  
     path('verifyOTPAPIView/',VerifyOTPAPIView.as_view(), name='verifyOTPAPIView'),  
     path('resetPasswordAPIView/',ResetPasswordAPIView.as_view(), name='resetPasswordAPIView'),  
-    path('notifi/',SendTestNotification.as_view(), name='notifi'),    
+    # path('notifi/',SendTestNotification.as_view(), name='notifi'), 
+    path('chat/<int:order_id>/', ChatMessageAPIView.as_view(), name='chat-message'),  
+    path('send-notification/', send_notification_view, name='send_notification'), 
 
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 
