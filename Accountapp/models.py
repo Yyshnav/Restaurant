177c89fd8e79dd296236348e2b45166bacb31c0e
Sayanthssk <sayanthskumar01@gmail.com>
2025-07-31 17:29:45 +0530

    added offer viewed delete edit pending

--- Accountapp/models.py
+++ Accountapp/models.py
@@ -236,7 +236,7 @@ class OfferTable(models.Model):
     offerdescription = models.CharField(max_length=100, null=True, blank=True)
     createdat = models.DateTimeField(auto_now_add=True)
     updatedat = models.DateTimeField(auto_now=True)
-
+    
     def __str__(self):
         return self.name
     
